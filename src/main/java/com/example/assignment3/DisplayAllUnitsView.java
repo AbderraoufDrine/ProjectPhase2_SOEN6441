89c4a562bf9a9f60034d60c498b14cb1c92b8de7
abderraoufdrine <abderraouf.drine@gmail.com>
2023-04-07 18:23:17 -0400

    Added Threading to DisplayAllUnitsView

--- src/main/java/com/example/assignment3/DisplayAllUnitsView.java
+++ src/main/java/com/example/assignment3/DisplayAllUnitsView.java
@@ -7,6 +7,7 @@ import com.example.assignment3.Database.TenantRepository;
 import com.example.assignment3.controllers.RentalUnitController;
 import com.example.assignment3.models.RentalUnit;
 
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -28,49 +29,56 @@ public class DisplayAllUnitsView {
 
     public void display() {
 
-        // Instantiate the TenantController
-        RentalUnitController rentalUnitController = new RentalUnitController(rentalUnitRepository,tenantRepository);
+        Thread thread = new Thread(() -> {
+            // Instantiate the TenantController
+            RentalUnitController rentalUnitController = new RentalUnitController(rentalUnitRepository,tenantRepository);
 
-        // Get the list of tenants
-        ArrayList<RentalUnit> units = rentalUnitController.displayAllUnits();
+            // Get the list of tenants
+            ArrayList<RentalUnit> units = rentalUnitController.displayAllUnits();
 
-        // Create the ListView to display the tenants
-        ListView<String> rentalUnitListView = new ListView<>();
+            // Create the ListView to display the tenants
+            ListView<String> rentalUnitListView = new ListView<>();
 
-        // Clear the ListView
-        rentalUnitListView.getItems().clear();
-
-        // Loop through the array and add each tenant to the ListView
-        for (RentalUnit unit : units) {
-            String unitInfo = unit.toString();
-            rentalUnitListView.getItems().add(unitInfo);
-        }
-
-        // Create the button to refresh the list of tenants
-        Button refreshTenantsBtn = new Button("Refresh");
-
-        // Create a VBox to hold the ListView and the Refresh button
-        VBox Box = new VBox(10, rentalUnitListView, refreshTenantsBtn);
-        Box.setPadding(new Insets(10));
-
-        // Create a new stage to show the tenants list
-        Stage rentalUnitStage = new Stage();
-        rentalUnitStage.setTitle("All Units");
-        //rentalUnitStage.initModality(Modality.APPLICATION_MODAL);
-        rentalUnitStage.initOwner(this.stage);
-        rentalUnitStage.setScene(new Scene(Box, 650, 325));
-        rentalUnitStage.show();
-
-        // Add event handler to the Refresh button
-        refreshTenantsBtn.setOnAction(event -> {
             // Clear the ListView
             rentalUnitListView.getItems().clear();
-            // Refresh the list of tenants and add them to the ListView
-            ArrayList<RentalUnit> units2 = rentalUnitController.displayAllUnits();
-            for (RentalUnit unit : units2) {
+
+            // Loop through the array and add each tenant to the ListView
+            for (RentalUnit unit : units) {
                 String unitInfo = unit.toString();
                 rentalUnitListView.getItems().add(unitInfo);
             }
+
+            // Create the button to refresh the list of tenants
+            Button refreshTenantsBtn = new Button("Refresh");
+
+            // Create a VBox to hold the ListView and the Refresh button
+            VBox Box = new VBox(10, rentalUnitListView, refreshTenantsBtn);
+            Box.setPadding(new Insets(10));
+
+            // Create a new stage to show the tenants list
+            Platform.runLater(()->{
+            Stage rentalUnitStage = new Stage();
+            rentalUnitStage.setTitle("All Units");
+            //rentalUnitStage.initModality(Modality.APPLICATION_MODAL);
+            rentalUnitStage.initOwner(this.stage);
+            rentalUnitStage.setScene(new Scene(Box, 650, 325));
+            rentalUnitStage.show();
+            });
+
+            // Add event handler to the Refresh button
+            refreshTenantsBtn.setOnAction(event -> {
+                // Clear the ListView
+                rentalUnitListView.getItems().clear();
+                // Refresh the list of tenants and add them to the ListView
+                ArrayList<RentalUnit> units2 = rentalUnitController.displayAllUnits();
+                for (RentalUnit unit : units2) {
+                    String unitInfo = unit.toString();
+                    rentalUnitListView.getItems().add(unitInfo);
+                }
+            });
         });
+
+        thread.setDaemon(true);
+        thread.start();
     }
 }
